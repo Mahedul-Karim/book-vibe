0149c0e727c70cf7d2687af6fdb49468de93110f
Mahedul-Karim <karimrupo21@gmail.com>
2025-04-17 09:23:07 +0600

    initial setup done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-6xl font-work bg-primary" >Hello World from vite </h1>
+      <Button variant={'secondary'} >Hello World</Button>
+    </>
+  );
+}
+
+export default App;
